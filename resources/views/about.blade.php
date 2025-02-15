2fc108ab96246a76bc3b4a6624b36dbe0d5eb553
vann19 <ahmadfansya660@gmail.com>
2025-02-15 10:38:05 +0700

    Blade Component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">blog</a>
-    <a href="/about">about</a>
-    <a href="/contact">contact</a>
-    <h1>Ini halaman about</h1>
-    <h3>Bebek {{ $name  }}</h3>
-    <img src="img/bebek3.jpg" alt="" width="200">
-</body>
-</html>
+<x-layout>
+   <x-slot:title>{{ $title }}</x-slot:title>
+  <h3>ini adalah halaman about</h3>
+  <p>Nama : {{ $name }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get(uri: '/about', action: function () {
-    return view('about',['name' => 'Laravel']);
+    return view('about',['name' => 'Fansya', 'title' => 'About']);
 });
 
 Route::get(uri: '/blog', action: function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get(uri: '/contact', action: function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
 
