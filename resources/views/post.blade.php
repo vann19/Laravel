87f8d210dcf15103e175e7d986dcb26ba030050d
vann19 <ahmadfansya660@gmail.com>
2025-02-22 11:22:35 +0700

    cara menambahkan slug wpu laravel #6 Done

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+   <x-slot:title>{{ $title }}</x-slot:title>     
+
+  <article class="py-8 max-w-screen-md">
+    <h2 class="mb-3 text-3xl tracking-tight font-bold text-gray-900">{{ $post
+    ['title'] }}</h2>
+    <div class="text-base text-gray-500">
+      <a href="#">{{ $post ['author'] }}</a> | Januari 1, 2023
+    </div>
+    <p class="my-4 font-light">{{ $post['body'] }}</p>
+    <a href="/posts" class="font-medium text-indigo-600 hover:text-indigo-500">&laquo; Back to Posts</a>
+  </article>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,6 +15,7 @@ Route::get(uri: '/posts', action: function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Fansyaode',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae aspernatur totam alias iusto, nihil molestias rerum incidunt, corrupti soluta illo, praesentium quas iure quasi modi commodi fuga? Nulla, expedita nihil.
@@ -23,6 +25,7 @@ Route::get(uri: '/posts', action: function () {
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Agus Buntung',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae aspernatur totam alias iusto, nihil molestias rerum incidunt, corrupti soluta illo, praesentium quas iure quasi modi commodi fuga? Nulla, expedita nihil.
@@ -31,6 +34,37 @@ Route::get(uri: '/posts', action: function () {
 
         ],
     ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Fansyaode',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae aspernatur totam alias iusto, nihil molestias rerum incidunt, corrupti soluta illo, praesentium quas iure quasi modi commodi fuga? Nulla, expedita nihil.
+             Doloribus iusto sunt, ut dicta corporis esse minus amet iste suscipit odit deserunt sint aut, error alias enim magni ea nemo. Corporis voluptate similique quos itaque vitae ut atque enim.
+             Sequi laborum earum reprehenderit doloribus sit dolores a voluptas blanditiis ipsum doloremque numquam labore illo officiis, recusandae ipsa, maxime voluptates atque veniam exercitationem! Hic voluptatem vel temporibus repellendus. Tempore, error?'
+
+        ],
+        [   
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Agus Buntung',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae aspernatur totam alias iusto, nihil molestias rerum incidunt, corrupti soluta illo, praesentium quas iure quasi modi commodi fuga? Nulla, expedita nihil.
+             Doloribus iusto sunt, ut dicta corporis esse minus amet iste suscipit odit deserunt sint aut, error alias enim magni ea nemo. Corporis voluptate similique quos itaque vitae ut atque enim.
+             Sequi laborum earum reprehenderit doloribus sit dolores a voluptas blanditiis ipsum doloremque numquam labore illo officiis, recusandae ipsa, maxime voluptates atque veniam exercitationem! Hic voluptatem vel temporibus repellendus. Tempore, error?'
+
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get(uri: '/contact', action: function () {
