0bd4188076366d1ac2c34c4ff90c66ecb8037ef4
vann19 <ahmadfansya660@gmail.com>
2025-03-02 11:55:30 +0700

    8. Database & Migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,7 +27,7 @@ class Post
             'author' => 'Agus Buntung',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae aspernatur totam alias iusto, nihil molestias rerum incidunt, corrupti soluta illo, praesentium quas iure quasi modi commodi fuga? Nulla, expedita nihil.
              Doloribus iusto sunt, ut dicta corporis esse minus amet iste suscipit odit deserunt sint aut, error alias enim magni ea nemo. Corporis voluptate similique quos itaque vitae ut atque enim.
-             Sequi laborum earum reprehenderit doloribus sit dolores a voluptas blanditiis ipsum doloremque numquam labore illo officiis, recusandae ipsa, maxime voluptates atque veniam exercitationem! Hic voluptatem vel temporibus repellendus. Tempore, error?'
+             Sequi laborum earum reprehenderit doloribus sit dolores a voluptas blanditiis ipsum doloremque numquam labore illo officiis, recusandae ipsa, maxime voluptates atque veniam exercitationem! Hic voluptatem vel temporibus repellendus. Tempore,'
 
         ],
     ];
